1b9515015ead5fd1f6e8fa7fb80c586ae2aab198
N.U <n.n.ushakova@gmail.com>
2020-10-28 12:22:17 -0400

    Handle error whyle fetching quote

--- script.js
+++ script.js
@@ -4,9 +4,10 @@ async function getQuote() {
     const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     try {
         const response = await fetch(proxyUrl + apiUrl);
-        const data = await response.json();
+        const data = await response.json(); /*parse json into JS object*/
         console.log(data);
     } catch(error) {
+        getQuote();
         console.log('no quote', error);
     }
 }
