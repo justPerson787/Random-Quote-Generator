7e964652ea50a7bac777381b20baff4796249388
N.U <n.n.ushakova@gmail.com>
2020-10-28 12:27:48 -0400

    Set dynamic content in function

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBTN = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
 // Get Quote From API
 async function getQuote() {
     const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
@@ -5,7 +11,8 @@ async function getQuote() {
     try {
         const response = await fetch(proxyUrl + apiUrl);
         const data = await response.json(); /*parse json into JS object*/
-        console.log(data);
+        authorText.innerText = data.quoteAuthor;
+        quoteText.innerText = data.quoteText;
     } catch(error) {
         getQuote();
         console.log('no quote', error);
